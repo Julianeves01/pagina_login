eb19fd9dee810e9804d67deedf790cb9fd3945aa
Julianeves01 <Julia.s.neves6@aluno.senai.br>
2025-03-06 10:58:22 -0300

    atualização da imagem

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,10 +1,9 @@
 export default function Image() {
     return (
     <img 
-        src="/imagemLogin.png"
+        src="/rosinha.jpg" 
         alt="Login Illustration" 
         style={{ width: "100%", height: "100%", objectFit: "cover" }} 
     />
-    )
+    );
 }
-    
