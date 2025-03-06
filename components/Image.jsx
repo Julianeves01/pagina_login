7f4d401522dbcec17873123661029a943cc21469
Julianeves01 <Julia.s.neves6@aluno.senai.br>
2025-03-06 10:59:53 -0300

    atualização da imagem

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image() {
     return (
     <img 
-        src="/rosinha.jpg" 
+        src="/rosaFundo.jpg" 
         alt="Login Illustration" 
         style={{ width: "100%", height: "100%", objectFit: "cover" }} 
     />
